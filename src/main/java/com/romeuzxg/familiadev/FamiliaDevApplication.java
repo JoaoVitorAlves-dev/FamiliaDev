08bb1ce19897611f1ff7b20a1db872a05ecd93f5
JoaoVitorAlves-dev <ytmarcos43@gmail.com>
2026-09-09 23:06:11 -0300

    feat: Adiciona arquitetura moderna.

---
+++ src/main/java/com/romeuzxg/familiadev/FamiliaDevApplication.java
@@ -0,0 +1,13 @@
+package com.romeuzxg.familiadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliaDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamiliaDevApplication.class, args);
+    }
+
+}
